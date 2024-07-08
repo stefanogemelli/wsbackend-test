4c8812b3357dc3aa30fd2977013a7b0a572e595b
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-07-08 18:32:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { config } from "dotenv";
+
+const { FRONTEND_URL } = config().parsed;
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const httpServer = createServer(server);
+const io = new Server(httpServer, {
+  cors: {
+    origin: FRONTEND_URL,
+    credentials: true,
+  },
+});
+// io.use((socket, next) => {
+//   cookieParser()(socket.request, socket.request, next);
+// });
+
+// io.on("connection", onConnection(io));
+
+io.on("connection", (socket) => {
+  console.log("connection socketId", socket.id);
+  socket.emit("hello", "world");
+
+  socket.on("message:new", (arg) => {
+    console.log("message", arg);
+    socket.broadcast.emit("message:sv", { id: arg.id, data: "sv_" + arg.data });
+  });
+});
+
+httpServer.listen("3001", () => {
+  console.log(`Server listening on PORT 3001`);
+});
